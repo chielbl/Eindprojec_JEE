e76f0de616b4a4bddd17bb3467bcad191344e0cf
chielbleyenbergh <chielbleynbergh@gmail.com>
2018-12-19 13:40:15 +0100

    last update: - CSS

--- src/main/java/ehb/be/eindproject/Controller/OrderController.java
+++ src/main/java/ehb/be/eindproject/Controller/OrderController.java
@@ -1,29 +1,19 @@
 package ehb.be.eindproject.Controller;
 
-import ehb.be.eindproject.model.Artikel;
-import ehb.be.eindproject.model.ArtikelRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class OrderController {
 
-    @Autowired
-    private ArtikelRepository artikelRepository;
-
-    @ModelAttribute("all")
-    public Iterable<Artikel> findAll() {
-        return artikelRepository.findAll();
-    }
-
     @RequestMapping(value = "/order", method = RequestMethod.GET)
     public String showOrder(ModelMap map) {
         return "order";
     }
+
 
 }
